c7612d57720876e5b57ca5492484423f1f5f49d0
Ross Williams <r.b.williams94@gmail.com>
2016-01-29 11:38:32 +0000

    Now displays startship information in html table

--- api.js
+++ api.js
@@ -7,12 +7,28 @@ $(document).ready(function(){
         url: "starships.csv",
         async: false,
         success: function (csvd){
-            data = $.csv.toArrays(csvd);
+            data = $.csv.toObjects(csvd);
         },
         dataType: "text",
         complete: function(){
+            populateTable(data);
             console.log(data);
         }
     })
 
 });
+
+function populateTable(data) {
+    var tableBody = $('#table'),
+        props = ["name", "manufacturer", "cost_in_credits", "length", "max_atmosphering_speed",
+            "cargo_capacity_kg", "hyperdrive_rating", "docking_station_latitide", "docking_station_longitude"];
+    $.each(data, function(i, info){
+        var tr = $('<tr>');
+        if (i != 0){
+            $.each(props, function(i, prop){
+                $('<td>').html(info[prop]).appendTo(tr);
+            });
+            tableBody.append(tr);
+        }
+    });
+}

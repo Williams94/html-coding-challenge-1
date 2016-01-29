1e4bcea5ba71452666ff64b55235127221c52193
Ross Williams <r.b.williams94@gmail.com>
2016-01-29 11:05:17 +0000

    Loaded and parsed CSV into Array

--- api.js
+++ api.js
@@ -1,3 +1,18 @@
 /**
  * Created by rbwilliams on 29/01/2016.
  */
+$(document).ready(function(){
+
+    $.ajax({
+        url: "starships.csv",
+        async: false,
+        success: function (csvd){
+            data = $.csv.toArrays(csvd);
+        },
+        dataType: "text",
+        complete: function(){
+            console.log(data);
+        }
+    })
+
+});

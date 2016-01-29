554484919f8ccad646740296052b87b82366615d
Ross Williams <r.b.williams94@gmail.com>
2016-01-29 15:53:31 +0000

    Users can now sort starships based on distance to their location, descending and ascending

--- api.js
+++ api.js
@@ -1,6 +1,11 @@
 /**
  * Created by rbwilliams on 29/01/2016.
  */
+
+var data;
+var lat;
+var long;
+
 $(document).ready(function(){
 
     $.ajax({
@@ -8,44 +13,65 @@ $(document).ready(function(){
         async: false,
         success: function (csvd){
             data = $.csv.toObjects(csvd);
+            userLocation(data);
         },
-        dataType: "text",
-        complete: function(){
-            //dropdownList(data);
-            populateTable(data);
-            sort(data);
-            //console.log(data);
-        }
+        dataType: "text"
     })
+
 
 });
 
 function populateTable(data) {
+    $('table > tbody > tr').remove();
+
     var tableBody = $('#table'),
         props = ["name", "manufacturer", "cost_in_credits", "length", "max_atmosphering_speed",
-            "cargo_capacity_kg", "hyperdrive_rating", "docking_station_latitide", "docking_station_longitude"];
+            "cargo_capacity_kg", "hyperdrive_rating", "docking_station_latitide", "docking_station_longitude", "distance"];
+
     $.each(data, function(i, info){
         var tr = $('<tr>');
             $.each(props, function(i, prop){
                 $('<td>').html(info[prop]).appendTo(tr);
             });
         tableBody.append(tr);
-
     });
 }
 
-function sort(data){
+function userLocation(data){
     var output = document.getElementById("output");
     if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            var lat = position.coords.latitude;
-            var long = position.coords.longitude;
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
             output.innerHTML = '<p>Latitude is ' + lat + '° <br>Longitude is ' + long + '°</p>';
-            console.log(position.coords.latitude, position.coords.longitude);
+            calculateDistance(lat, long);
+            //sort(lat, long);
+            populateTable(data);
         });
     } else {
 
     }
+}
+
+function calculateDistance(lat, long){
+    $.each(data, function(i, location){
+        data[i].distance = distance(lat, long, location["docking_station_latitide"], location["docking_station_longitude"]).toString();
+    });
+}
+
+function sortAsc(){
+    data.sort(function(a, b){
+        return distance(lat, long, a["docking_station_latitide"], a["docking_station_longitude"]) -
+            distance(lat, long, b["docking_station_latitide"], b["docking_station_longitude"]);
+    });
+    populateTable(data);
+}
+function sortDes(){
+    data.sort(function(a, b){
+        return distance(lat, long, b["docking_station_latitide"], b["docking_station_longitude"]) -
+                distance(lat, long, a["docking_station_latitide"], a["docking_station_longitude"]);
+    });
+    populateTable(data);
 }
 
 function distance(lat1, lon1, lat2, lon2){
@@ -58,8 +84,12 @@ function distance(lat1, lon1, lat2, lon2){
     var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
 
     dist = Math.acos(dist);
-    dist =
+    dist = dist * 180/Math.PI;
+    dist = dist * 60 * 1.1515;
 
+    //console.log(dist);
+
+    return dist;
 }
 
 /*
